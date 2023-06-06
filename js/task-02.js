3ae9ad4d96c9f5370fbbd17d6682cccc495bdf84
Yulii Kinash <122307040+yulii0@users.noreply.github.com>
2023-06-06 04:51:46 -0400

    Task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,11 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const list = document.querySelector('#ingredients');
+const elements = ingredients.map(element => {
+  const li = document.createElement('li');
+  li.textContent = element;
+  li.classList.add("item");
+  return li;
+});
+list.append(...elements);
