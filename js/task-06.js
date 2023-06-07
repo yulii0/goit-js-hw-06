bceccf03084fa2087e8909b083686628d4737504
Yulii Kinash <122307040+yulii0@users.noreply.github.com>
2023-06-07 08:12:51 -0400

    Task 6

---
+++ js/task-06.js
@@ -0,0 +1,13 @@
+const input = document.querySelector('#validation-input');
+console.dir(input);
+
+input.addEventListener("blur", evt => {
+    console.dir(evt.currentTarget.value.length);
+    if (Number(evt.currentTarget.value.trim().length) === Number(input.dataset.length)) {
+        input.classList.add('valid');
+        input.classList.remove('invalid');
+        return;
+    } 
+    input.classList.add('invalid');
+});
+
