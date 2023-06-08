26c4d0f2758b3918f25a17446154994212c493d1
Yulii Kinash <122307040+yulii0@users.noreply.github.com>
2023-06-08 06:35:00 -0400

    Task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,18 @@
+const colorText = document.querySelector('.color');
+const btn = document.querySelector('.change-color');
+
+btn.addEventListener('click',onClick);
+function onClick(evt){
+  const randomColor = getRandomHexColor();
+  colorText.textContent = `${randomColor}`
+  document.body.style.backgroundColor = `${randomColor}`;
+}
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+
+
