b532a12ac135e28c8c7e93efda21816b29237593
Yulii Kinash <122307040+yulii0@users.noreply.github.com>
2023-06-08 06:15:21 -0400

    Task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const logInForm = document.querySelector('.login-form');
+
+logInForm.addEventListener('submit', onSubmit);
+function onSubmit(evt){
+    evt.preventDefault();
+    const message = "Заповни всі поля!";
+    const {email,password} = evt.currentTarget.elements;
+    if (email.value === "" || password.value === "") {
+    alert(message);
+    return;
+    }
+        const userData = {
+            email : email.value,
+            password : password.value
+        };
+        console.log(userData);
+        logInForm.reset();  
+}
 
